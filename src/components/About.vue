7f806070aa18760065edd6e3f1314638b7b77b09
Juan Diego López <juandlopeztriana@gmail.com>
2018-08-19 00:44:05 -0500

    Bugs fixed

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,7 +3,7 @@
         <v-parallax src="public/section1.jpg" height="380">
             <v-layout row wrap align-center>
                 <v-flex xs12>
-                    <div class="headline white--text lg-3 text-xs-center pa-5">
+                    <div class="headline white--text lg-3 text-xs-center">
                         I am a student of Systems Engineering last semester at the Universidad Industrial de Santander. In recent years I have focused my studies on software development, especially in the area of web applications. I am someone responsible, dedicated, with the ability to work as a team, lover of technology and with a desire to continue learning every day.
                     </div>
                 </v-flex>
@@ -13,5 +13,9 @@
 </template>
 
 <style scoped>
-
+@media screen and (max-width: 500px){
+    .headline {
+        font-size: 16px !important;
+    }
+}
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,22 +1,51 @@
 <template>
-    <v-toolbar dark>
-        <v-toolbar-title>
-            <h3 class="name"> {{title}} </h3>
-        </v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-toolbar-items>
-            <v-btn flat="flat" @click="$vuetify.goTo('#tech')">Techs</v-btn>
-            <v-btn flat="flat" @click="$vuetify.goTo('#about')">About Me</v-btn>
-            <v-btn flat="flat" @click="$vuetify.goTo('#contact')">Contact</v-btn>
-        </v-toolbar-items>
-    </v-toolbar>
+    <div>
+        <v-toolbar dark>
+            <v-toolbar-side-icon class="hidden-sm-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title>
+                <h3 class="name"> {{title}} </h3>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items class="hidden-xs-only">
+                <v-btn flat="flat" @click="goTo('#tech')">Techs</v-btn>
+                <v-btn flat="flat" @click="goTo('#about')">About Me</v-btn>
+                <v-btn flat="flat" @click="goTo('#contact')">Contact</v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+        <v-navigation-drawer v-model="drawer" absolute dark temporary>
+            <v-list>
+                <v-list-tile @click="goTo('#tech')">
+                    <v-list-tile-content>
+                        <v-list-tile-title>TECHS</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile @click="goTo('#about')">
+                    <v-list-tile-content>
+                        <v-list-tile-title>ABOUT ME</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile @click="goTo('#contact')">
+                    <v-list-tile-content>
+                        <v-list-tile-title>CONTACT</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+    </div>
 </template>
 
 <script>
 export default {
     data () {
         return {
-            title: 'Juan Diego López'
+            title: 'Juan Diego López',
+            drawer: false
+        }
+    },
+    methods: {
+        goTo (element) {
+            this.drawer = false
+            this.$vuetify.goTo(element)
         }
     }
 }
