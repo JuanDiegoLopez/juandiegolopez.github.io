3bcdd25d457fc488ded393daf6ea1bf6f5368d1c
Juan Diego López <juandlopeztriana@gmail.com>
2018-08-23 11:24:14 -0500

    Profile photo changed

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
     <v-parallax src="public/section1.jpg" height="600">
         <v-layout column align-center justify-center class="white--text">
         <v-avatar size="200">
-            <img src="public/foto.jpeg" alt="photo.js" height="200">
+            <img src="public/profile_photo.png" alt="photo.js" height="200">
         </v-avatar>
         <br>
         <h1 class="white--text mb-2 display-1 text-xs-center font-weight-black">Fullstack Developer</h1>
