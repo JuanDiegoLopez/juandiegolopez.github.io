cbe7ce6f976897a04dc351ccea51ae8d1d1cb813
Juan Diego López <juandlopeztriana@gmail.com>
2018-08-23 11:55:57 -0500

    Professional profile changed

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,7 +4,7 @@
             <v-layout row wrap align-center>
                 <v-flex xs12>
                     <div class="headline white--text lg-3 text-xs-center">
-                        I am a student of Systems Engineering last semester at the Universidad Industrial de Santander. In recent years I have focused my studies on software development, especially in the area of web applications. I am someone responsible, dedicated, with the ability to work as a team, lover of technology and with a desire to continue learning every day.
+                        I am a Systems Engineering student at Universidad Industrial de Santander. In the last years of my career, I focused my studies in software development, especially in the web applications area. I am someone dedicated, responsible, with the ability to work as a team, lover of what I do and with the disre to continue learning every day.
                     </div>
                 </v-flex>
             </v-layout>
